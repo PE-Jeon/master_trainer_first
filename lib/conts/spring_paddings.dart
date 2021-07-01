4c5c2d67ae255e5cc8978f678f88d77c88fa46c0
전준현 <info@frentree.com>
2021-07-01 21:39:17 +0900

    Clean to start with GetX

--- lib/conts/spring_paddings.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Padding verticalSpaceSmall = Padding(padding: EdgeInsets.symmetric(vertical: 5.0));
-Padding verticalSpaceMedium = Padding(padding: EdgeInsets.symmetric(vertical: 7.0));
-Padding verticalSpaceLarge = Padding(padding: EdgeInsets.symmetric(vertical: 10.0));
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,15 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:master_trainer_first/example_ui.dart';
-import 'package:master_trainer_first/repos/locator.dart';
-import 'package:master_trainer_first/repos/router.gr.dart' as auto_router;
-import 'package:stacked_services/stacked_services.dart';
 
 void main() async {
-  setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  final _router = auto_router.Router();
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: kcPrimaryColor,
-      ),
-
-      initialRoute: auto_router.Routes.loginView,
-      onGenerateRoute: auto_router.Router().onGenerateRoute,
-      navigatorKey: locator<NavigationService>().navigatorKey,
     );
   }
 }
